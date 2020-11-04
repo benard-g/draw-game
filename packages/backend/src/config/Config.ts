8636037b4085c92c6f47f52ea834099404b11b0a
Guillaume Benard <guillaume@benard.io>
2020-11-04 16:05:58 +0100

    config

--- packages/backend/src/config/Config.ts
+++ packages/backend/src/config/Config.ts
@@ -1,3 +1,5 @@
+import { getFromEnv, getNumberFromEnv } from '../utils/envUtils';
+
 export interface Config {
   NODE_ENV: 'production' | 'test' | 'development';
 
@@ -5,10 +7,22 @@ export interface Config {
 }
 
 export function loadConfig(): Config {
-  // TODO: load from env
   return {
-    NODE_ENV: 'development',
+    NODE_ENV: getNodeEnv(),
 
-    PORT: 8080,
+    PORT: getNumberFromEnv('PORT', 8080),
   };
+}
+
+function getNodeEnv(): 'production' | 'test' | 'development' {
+  const nodeEnv = getFromEnv('NODE_ENV', null);
+
+  switch (nodeEnv) {
+    case 'production':
+      return 'production';
+    case 'test':
+      return 'test';
+    default:
+      return 'development';
+  }
 }
